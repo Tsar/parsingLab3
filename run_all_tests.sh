f10f33db0d43ff27cb3bc3b8e09229696fab4658
Ioann Volkov <volkov.ioann@gmail.com>
2012-05-30 14:32:17 +0400

    Add one more incorrect test

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CLASSPATH="/usr/share/java/antlr3.jar:./" java Test test01 test02 test_incorrect
+CLASSPATH="/usr/share/java/antlr3.jar:./" java Test test01 test02 test_incorrect_1 test_incorrect_2
